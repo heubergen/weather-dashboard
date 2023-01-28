6ada5a9cbc1025d43e66116b172e5ace5d718b32
Patrick <github@z33.ch>
2023-01-28 23:09:29 +0100

    Add fake favicon
    To fix 404 with FF

--- scripts/dashboard-server.sh
+++ scripts/dashboard-server.sh
@@ -27,6 +27,7 @@ do
                 <head>
                         <meta name="viewport" content="width=device-width, initial-scale=1">
                         <meta charset="UTF-8">
+			<link rel="icon" type="image/png" href="data:image/png;base64,iVBORw0KGgo=">
                         <title>
                                 Weather Dasboard
                         </title>
