6920fcbb9388f1b010e62033cbe340fe19726d3e
Patrick Albrecht <github@z33.ch>
2023-02-17 22:27:31 +0100

    Replace js with bash solution to calculate countdown

--- scripts/dashboard-server.sh
+++ scripts/dashboard-server.sh
@@ -21,6 +21,28 @@ pitempf=$(sudo /usr/bin/vcgencmd measure_temp | awk -F "[=']" '{print($2 * 1.8)+
 piload=$(cut -f 1 -d " " /proc/loadavg)
 pimemory=$(awk '/MemFree/ { printf "%.0f \n", $2/1024 }' /proc/meminfo | sed 's/ *$//')
 
+dates=('YYYY-MM-DD' 'YYYY-MM-DD' 'YYYY-MM-DD')
+now=$(date +'%Y-%m-%d')
+count=0
+
+if [[ $OSTYPE == 'darwin'* ]]; then
+	for date in "${dates[@]}"; do
+		diff=$((`date -jf %Y-%m-%d $date +%s` - `date -jf %Y-%m-%d $now +%s`))
+		days[count]="$(($diff/ 86400))"
+		weeks[count]="$(($diff/ 604800))"
+		count=$(( $count + 1 ))
+	done
+elif [[ $OSTYPE == 'linux'* ]]; then
+	for date in "${dates[@]}"; do
+		let diff=(`date +%s -d $date`-`date +%s -d $now`)
+		days[count]="$(($diff/ 86400))"
+		weeks[count]="$(($diff/ 604800))"
+		count=$(( $count + 1 ))
+	done
+else
+	echo "OS not supported, please patch script"
+fi
+
 touch /var/www/html/index.html
 
 cat > /var/www/html/index.html <<- EOF
