378c1f31535150117638550845ccc82c1dc51822
Patrick Albrecht <github@z33.ch>
2023-02-26 21:18:23 +0100

    Multiple fixes and improvements * Fix spellcheck findings * Separate reused code into function * Add calculation since birth

--- scripts/dashboard-server.sh
+++ scripts/dashboard-server.sh
@@ -3,14 +3,14 @@ set -Eeuo pipefail
 
 wfilename="/opt/meteo-exporter/data.txt"
 wcounter=0
-while read wline; do
+while read -r wline; do
 	wlines[wcounter]="$wline"
 	wcounter=$((wcounter+1))
 done < $wfilename
 
 ifilename="/opt/co2-exporter/data.txt"
 icounter=0
-while read iline; do
+while read -r iline; do
 	ilines[icounter]="$iline"
 	icounter=$((icounter+1))
 done < $ifilename
@@ -21,12 +21,21 @@ pimemory=$(awk '/MemFree/ { printf "%.0f \n", $2/1024 }' /proc/meminfo | sed 's/
 
 dates=('YYYY-MM-DD' 'YYYY-MM-DD' 'YYYY-MM-DD')
 now=$(date +'%Y-%m-%d')
-birth=('YYYY-MM-DD')
+birth='YYYY-MM-DD'
 count=0
 
 command_not_installed () {
 	echo "Please install $1 and make sure it's included in your PATH"
 	exit
+}
+
+set_dates () {
+	days[count]="$diff"
+	if [ "$1" = true ] ; then
+		birthd[count]=$((diffbirth / 365))
+	fi
+	weekstmp=$(echo "$diff"/ 7 | bc -l)
+	weeks[count]=$(printf "%.1f" "$weekstmp")
 }
 
 if command -v bc &> /dev/null; then
@@ -43,14 +52,14 @@ else
 	command_not_installed dateutils
 fi
 for date in "${dates[@]}"; do
-	diff=$($datediffcommand $now $date)
-	diffbirth=$($datediffcommand $birth $date)
-	days[count]="$diff"
-	birthd[count]=$(($diffbirth / 365))
-	weekstmp=$(echo $diff/ 7 | bc -l)
-	weeks[count]=$(printf "%.1f" $weekstmp)
-	count=$(( $count + 1 ))
+	diff=$("$datediffcommand" "$now" "$date")
+	diffbirth=$("$datediffcommand" "$birth" "$date")
+	set_dates true
+	count=$(( count + 1 ))
 done
+
+diff=$("$datediffcommand" "$now" "$birth")
+set_dates false
 
 touch /var/www/html/index.html
 
